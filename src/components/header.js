ee88262664dd9b8717fc93ae1b7e9b1fbeb45bf7
niclashaumann <niclashaumann@icloud.com>
2023-08-20 08:37:16 +0200

    404 issues

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 import "./header.css";
-import Logo from "../assets/logo.png";
+import Logo from "../assets/logo.svg";
 import { Link } from "react-router-dom";
 
 export default function Header() {
