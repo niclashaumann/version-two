cb26009d3348c4ac7e6b8a0c89fd092cb59b516b
niclashaumann <niclashaumann@icloud.com>
2023-08-20 21:24:52 +0200

    404 (yes!)

--- src/pages/noPage.js
+++ src/pages/noPage.js
@@ -1,10 +1,20 @@
+import "./noPage.css";
 import Header from "../components/header";
+import Footer from "../components/footer";
+import Error from "../assets/404.gif";
 
 export default function NoPage() {
   return (
     <div>
-      <Header />
-      <p>Error 404: Not found</p>
+      <div>
+        <Header />
+      </div>
+      <div className="wrapper-error">
+        <img src={Error} className="error-nopage" alt="404" />
+      </div>
+      <div>
+        <Footer />
+      </div>
     </div>
   );
 }
